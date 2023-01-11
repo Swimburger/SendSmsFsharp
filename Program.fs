b65eee25fadffa3c87ecbfb684e3c16a32b9edc0
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-01-11 10:16:07 -0500

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿open System.Reflection
+open Microsoft.Extensions.Configuration
+open Twilio
+open Twilio.Rest.Api.V2010.Account
+open Twilio.Types
+
+let configuration =
+    ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build()
+
+let twilioAccountSid = configuration["Twilio:AccountSid"]
+let twilioAuthToken = configuration["Twilio:AuthToken"]
+
+TwilioClient.Init(twilioAccountSid, twilioAuthToken)
+
+MessageResource.Create(
+    from = PhoneNumber("[YOUR_TWILIO_PHONE_NUMBER]"),
+    ``to`` = PhoneNumber("[YOUR_PERSONAL_PHONE_NUMBER]"),
+    body = "Ahoy!"
+)
+|> ignore
