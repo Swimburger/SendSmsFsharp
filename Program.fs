0ecd4143782d81423bd4df41683edc7e2c170ef0
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-01-17 17:57:08 -0500

    Match tutorial content

--- Program.fs
+++ Program.fs
@@ -1,20 +1,29 @@
-﻿open System.Reflection
+﻿open System
+open System.Reflection
 open Microsoft.Extensions.Configuration
 open Twilio
 open Twilio.Rest.Api.V2010.Account
 open Twilio.Types
 
-let configuration =
-    ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build()
+let configuration = ConfigurationBuilder()
+                     .AddUserSecrets(Assembly.GetExecutingAssembly())
+                     .Build()
 
 let twilioAccountSid = configuration["Twilio:AccountSid"]
 let twilioAuthToken = configuration["Twilio:AuthToken"]
 
+printfn "Twilio Account SID: %s"
+    (if String.IsNullOrEmpty(twilioAccountSid) then "[NOT CONFIGURED]" else twilioAccountSid)
+printfn "Twilio Auth Token: %s"
+    (if String.IsNullOrEmpty(twilioAuthToken) then "[NOT CONFIGURED]" else "[CONFIGURED]")
+    
 TwilioClient.Init(twilioAccountSid, twilioAuthToken)
 
-MessageResource.Create(
+let message = MessageResource.Create(
     from = PhoneNumber("[YOUR_TWILIO_PHONE_NUMBER]"),
     ``to`` = PhoneNumber("[YOUR_PERSONAL_PHONE_NUMBER]"),
     body = "Ahoy!"
 )
-|> ignore
+
+printfn "Message sent"
+printfn $"Message ID: {message.Sid}"
